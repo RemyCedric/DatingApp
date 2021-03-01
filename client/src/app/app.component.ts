eff03929b362fa65068fb6d0ab542a3f81beac22
RemyCedric <RemyCedric@outlook.com>
2021-03-01 06:12:36 +0100

    added httpClientModule, first get call from the client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
+interface User {
+  id: number;
+  username: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
-  x = '';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.http //
+      .get<User[]>('https://localhost:5001/api/users')
+      .subscribe(
+        (users: User[]) => {
+          this.users = users;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
